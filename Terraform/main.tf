1d57b753bfb1d222b21e00e767d46059fb5a0481
Kiru1027 <kirubanandni2403@gmail.com>
2024-12-19 14:50:26 +0530

    terraform file added

---
+++ Terraform/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.14.0"
+    }
+  }
+}
+ 
+provider "azurerm" {
+ 
+ 
+  client_id       = var.client_id         # The Application (client) ID
+  client_secret   = var.client_secret      # The client secret you created
+  tenant_id       = var.tenant_id          # The Directory (tenant) ID
+  subscription_id = var.subscription_id    # Your Azure subscription ID
+  features {}
+}
