42b1353d6e0515c9890f5fcb003d15779424cd1b
Kiru1027 <kirubanandni2403@gmail.com>
2024-12-19 19:33:03 +0530

    Adding storage account

--- Terraform/rg.tf
+++ Terraform/rg.tf
@@ -1,4 +1,4 @@
-resource "azurerm_resource_group" "rg"{
-    name = "Kirubate"
-    location = "East US"
+resource "azurerm_resource_group" "rg" {
+  name     = "Kirubate"
+  location = "East US"
 }
--- Terraform/var.tf
+++ Terraform/var.tf
@@ -2,19 +2,19 @@ variable "client_secret" {
   description = "Client secret for Azure"
   type        = string
 }
- 
+
 variable "client_id" {
   description = "Client ID for Azure"
   type        = string
 }
- 
+
 variable "tenant_id" {
-    description = "Tennat ID for Azure"
-    type = string
+  description = "Tennat ID for Azure"
+  type        = string
 }
- 
+
 variable "subscription_id" {
-    description = "subscription ID for Azure"
-    type = string
- 
+  description = "subscription ID for Azure"
+  type        = string
+
 }
