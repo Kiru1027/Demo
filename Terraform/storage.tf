3334248da9896ae7d0666578c713ecf593c6af7b
Kiru1027 <kirubanandni2403@gmail.com>
2024-12-19 21:07:30 +0530

    adding text

--- Terraform/storage.tf
+++ Terraform/storage.tf
@@ -4,8 +4,19 @@ resource_group_name = azurerm_resource_group.rg.name
 location = azurerm_resource_group.rg.location
 account_tier = "Standard"
 account_replication_type = "LRS"
-tags ={
-    environment = "staging"
+public_network_access_enabled = false
 }
+
+resource "azurerm_storage_container" "data" {
+name = "data"
+storage_account_name = "san2403"
+container_access_type = "private"
 }
-  
+resource "azurerm_storage_blob" "sample"{
+name = "sample.txt"
+storage_account_name = "san2403"
+storage_container_name = "data"
+type = "Block"
+source = "sample.txt"
+}
+
