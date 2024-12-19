2ab1fd59ece3a3f8137c978ec8f6695bcf0f4a0e
Kiru1027 <kirubanandni2403@gmail.com>
2024-12-19 23:28:32 +0530

    add

--- Terraform/storage.tf
+++ Terraform/storage.tf
@@ -1,5 +1,5 @@
-resource "azurerm_storage_account" "san2403" {
-  name                          = "san2403"
+resource "azurerm_storage_account" "san1027" {
+  name                          = "san1027"
   resource_group_name           = azurerm_resource_group.rg.name
   location                      = azurerm_resource_group.rg.location
   account_tier                  = "Standard"
@@ -7,14 +7,14 @@ resource "azurerm_storage_account" "san2403" {
   public_network_access_enabled = false
 }
 
-resource "azurerm_storage_container" "data" {
-  name                  = "data"
-  storage_account_name  = azurerm_storage_account.san2403.name
+resource "azurerm_storage_container" "mydata" {
+  name                  = "mydata"
+  storage_account_name  = azurerm_storage_account.san1027.name
   container_access_type = "private"
 }
 resource "azurerm_storage_blob" "sample" {
   name                   = "sample.txt"
-  storage_account_name   = azurerm_storage_account.san2403.name
+  storage_account_name   = azurerm_storage_account.san1027.name
   storage_container_name = azurerm_storage_container.data.name
   type                   = "Block"
   source                 = "./sample.txt"
